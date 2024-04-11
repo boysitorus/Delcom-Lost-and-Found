c9b93916a0eaf3d8eb4b438499e19d23b6a720b0
boyysitorus <sitorusboy0123@gmail.com>
2024-04-11 18:56:44 +0700

    praktikum 9

---
+++ app/src/main/java/com/ifs21025/lostandfound/data/repository/LostFoundRepository.kt
@@ -0,0 +1,149 @@
+package com.ifs21025.lostandfound.data.repository
+
+import com.google.gson.Gson
+import com.ifs21025.lostandfound.data.remote.MyResult
+import com.ifs21025.lostandfound.data.remote.response.DelcomResponse
+import com.ifs21025.lostandfound.data.remote.retrofit.IApiService
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+
+class LostFoundRepository private constructor(
+    private val apiService: IApiService,
+) {
+    fun postLostFound(
+        title: String,
+        description: String,
+        status: String
+    ) = flow {
+        emit(MyResult.Loading)
+        try {
+            //get success message
+            emit(
+                MyResult.Success(
+                    apiService.postLostFound(title, description, status).data
+                )
+            )
+        } catch (e: HttpException) {
+            //get error message
+            val jsonInString = e.response()?.errorBody()?.string()
+            emit(
+                MyResult.Error(
+                    Gson()
+                        .fromJson(jsonInString, DelcomResponse::class.java)
+                        .message
+                )
+            )
+        }
+    }
+
+    fun putLostFound(
+        lostFoundId: Int,
+        title: String,
+        description: String,
+        status: String,
+        isCompleted: Boolean,
+    ) = flow {
+        emit(MyResult.Loading)
+        try {
+            //get success message
+            emit(
+                MyResult.Success(
+                    apiService.putLostFound(
+                        lostFoundId,
+                        title,
+                        description,
+                        status,
+                        if (isCompleted) 1 else 0
+                    )
+                )
+            )
+        } catch (e: HttpException) {
+            //get error message
+            val jsonInString = e.response()?.errorBody()?.string()
+            emit(
+                MyResult.Error(
+                    Gson()
+                        .fromJson(jsonInString, DelcomResponse::class.java)
+                        .message
+                )
+            )
+        }
+    }
+
+    fun getLostFounds(
+        isCompleted: Int?,
+        isMe: Int = 1,
+        status: String,
+    ) = flow {
+        emit(MyResult.Loading)
+        try {
+            //get success message
+            emit(MyResult.Success(apiService.getTodos(isCompleted, isMe, status)))
+        } catch (e: HttpException) {
+            //get error message
+            val jsonInString = e.response()?.errorBody()?.string()
+            emit(
+                MyResult.Error(
+                    Gson()
+                        .fromJson(jsonInString, DelcomResponse::class.java)
+                        .message
+                )
+            )
+        }
+    }
+
+    fun getLostFound(
+        lostFoundId: Int,
+    ) = flow {
+        emit(MyResult.Loading)
+        try {
+            //get success message
+            emit(MyResult.Success(apiService.getTodo(lostFoundId)))
+        } catch (e: HttpException) {
+            //get error message
+            val jsonInString = e.response()?.errorBody()?.string()
+            emit(
+                MyResult.Error(
+                    Gson()
+                        .fromJson(jsonInString, DelcomResponse::class.java)
+                        .message
+                )
+            )
+        }
+    }
+
+    fun deleteLostFound(
+        lostFoundId: Int,
+    ) = flow {
+        emit(MyResult.Loading)
+        try {
+//get success message
+            emit(MyResult.Success(apiService.deleteTodo(lostFoundId)))
+        } catch (e: HttpException) {
+            //get error message
+            val jsonInString = e.response()?.errorBody()?.string()
+            emit(
+                MyResult.Error(
+                    Gson()
+                        .fromJson(jsonInString, DelcomResponse::class.java)
+                        .message
+                )
+            )
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LostFoundRepository? = null
+        fun getInstance(
+            apiService: IApiService,
+        ): LostFoundRepository {
+            synchronized(LostFoundRepository::class.java) {
+                INSTANCE = LostFoundRepository(
+                    apiService
+                )
+            }
+            return INSTANCE as LostFoundRepository
+        }
+    }
+}
